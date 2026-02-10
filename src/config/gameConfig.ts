d72e5226017c0bb96ff014aba72c3f73468e371e
Patryk Łowigus <101129079+plowigus@users.noreply.github.com>
2026-02-10 20:00:44 +0100

    dev

--- src/config/gameConfig.ts
+++ src/config/gameConfig.ts
@@ -10,7 +10,7 @@ export const COLLISION_CATEGORIES = {
 export const GAME_CONFIG = {
     width: 1080,
     height: 450,
-    debugMode: true,
+    debugMode: false,
     COMMODORE_64_INTRO: false, // Set to true to enable the C64 intro loader
     moveSpeed: 7,
     maxMoveSpeed: 12,
